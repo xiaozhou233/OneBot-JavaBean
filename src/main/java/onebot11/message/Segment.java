1fedf1452b3cba48ba385a8dc42f43dbfd39025b
xiaozhou233 <xiaozhou233@vip.qq.com>
2025-05-24 05:26:18 +0800

    Bug Fix

--- src/main/java/onebot11/message/Segment.java
+++ src/main/java/onebot11/message/Segment.java
@@ -7,7 +7,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.HashMap;
 
 @Data
-@EqualsAndHashCode(callSuper = true)
 public class Segment {
     @JsonProperty("type")
     public String type;
