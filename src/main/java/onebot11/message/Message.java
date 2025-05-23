d8874f207a156b583f21f0e8d1ac486d4d5d259e
xiaozhou233 <xiaozhou233@vip.qq.com>
2025-05-24 04:24:17 +0800

    Feat - Message Segment

--- src/main/java/onebot11/message/Message.java
+++ src/main/java/onebot11/message/Message.java
@@ -1,10 +1,15 @@
 package onebot11.message;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Data;
+import java.util.List;
 
-@Data
 public class Message {
     // TODO: Message Segments
-    public Object[] message;
+    public List<Segment> segments;
+
+    public void setMessage(Object[] message) {
+        
+    }
+
+    
 }
